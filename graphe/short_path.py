a5b7c62760dc4de4bf7566dea691f625e8b0cf45
ravaka34 <ravakanyaina34@gmail.com>
2024-07-31 13:19:13 +0300

    update print short path

--- graphe/short_path.py
+++ graphe/short_path.py
@@ -1,15 +1,15 @@
 """
     Algorithm to know the short path from a specific vertex to the other vertices
 """
-def moore_djikstra(oriented_graph, start_index):
+def moore_djikstra(oriented_graph, start_index, vertex_name):
     #Init all the vertices pi values to infinit except for the start index
     vertex_number = len(oriented_graph[0])
     pi_values = [[float('inf'), ''] for _ in range(vertex_number)]
-    pi_values[start_index] = [0, f'{start_index}']
+    pi_values[start_index] = [0, vertex_name[start_index]]
     not_visited = { i : float('inf') for i in range(vertex_number) }
     not_visited[start_index] = 0
+    print
 
-    print(pi_values)
     while not_visited:
         # current_index is min pi_values and is not_visited
         current_index = min(not_visited, key = not_visited.get)
@@ -20,7 +20,7 @@ def moore_djikstra(oriented_graph, start_index):
             if distance != 0:
                 pi_value = distance + current_pi
                 if pi_value < pi_values[i][0]:
-                    pi_values[i] = [pi_value, f'{pi_values[current_index][1]}>{i}']
+                    pi_values[i] = [pi_value, f'{pi_values[current_index][1]}>'+vertex_name[i]]
                     not_visited[i] = pi_value
         not_visited.pop(current_index)
        
@@ -79,13 +79,18 @@ test_cycle_absorbant = [
     [1, 0, 0]
 ]
 
-# print(moore_djikstra(oriented_graph, 0))
-print(bellman_ford(test_cycle_absorbant, 0))
-    
-
-
-
-                
-
-
-
+exo = [
+    [0, 4, 6, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 6, 2, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 3, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 4, 0, 0, 0, 6, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 8, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 7],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+vertex_name = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+print(moore_djikstra(exo, 0, vertex_name))
+# print(bellman_ford(test_cycle_absorbant, 0))
